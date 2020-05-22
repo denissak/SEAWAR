c8a3d9f84afdd7b56031d3c737c24a8e7e6c362f
Denis <denissak9@gmail.com>
2020-05-22 15:06:44 +0300

    SEA WAR

---
+++ src/TestSimpleDotCom.java
@@ -0,0 +1,19 @@
+public class TestSimpleDotCom
+{
+    public static void main (String [] args)
+    {
+        SimpleDotCom dot = new SimpleDotCom();
+        int [] locations = {2, 3, 4};
+        dot.setLocationsSells(locations);
+        String userGuess = "2";
+        String result = dot.checkYourself(userGuess);
+        String testResult = "Неудача";
+        if (result.equals("Попал"))
+        {
+            testResult = "Пройден";
+        }
+        System.out.println(testResult);
+    }
+
+
+}
