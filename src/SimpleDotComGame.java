36bb9c59cb70a0481782294947e42f7c2df0d3e0
Denis <denissak9@gmail.com>
2020-05-25 10:04:06 +0300

    Release

--- src/SimpleDotComGame.java
+++ src/SimpleDotComGame.java
@@ -1,9 +1,10 @@
+/*
 public class SimpleDotComGame {
 
     public static void main (String[] args) {
         int numOfGuesses = 0;
         GameHelper helper = new GameHelper();
-        SimpleDotCom theDotCom = new SimpleDotCom();
+        DotCom theDotCom = new DotCom();
         int randomNum = (int) (Math.random() * 5);
         int[] locations = {randomNum, randomNum + 1, randomNum + 2};
         theDotCom.setLocationCells(locations);
@@ -21,3 +22,4 @@ public class SimpleDotComGame {
         }
     }
 }
+*/
